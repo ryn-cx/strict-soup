4dab692c190c1890afd2d0d084388875eeeeb8b9
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2023-12-24 23:30:27 -0800

    Improved docstring and made code more Pythonic (raise in if, return otherwise)

--- strict_soup/__init__.py
+++ strict_soup/__init__.py
@@ -11,7 +11,7 @@ from bs4.element import ResultSet, Tag
 
 
 class StrictSelectError(Exception):
-    """Exception raised when a strict_* function  fails to find a match."""
+    """Exception raised when a strict_* function fails to find a match."""
 
 
 class StrictMixin(Tag):
@@ -22,30 +22,22 @@ class StrictMixin(Tag):
     def select(
         self,
         selector: str,
-        namespaces: Any | None = None,  # noqa: ANN401 - These types are copied directly from the parent class
+        namespaces: Any | None = None,  # noqa: ANN401 - This type is copied directly from the parent class
         limit: int | None = None,
-        **kwargs: Any,  # noqa: ANN401 - These types are copied directly from the parent class
+        **kwargs: Any,  # noqa: ANN401 - This type is copied directly from the parent class
     ) -> ResultSet[StrictTag]:
-        """.select that returns a ResultSet of StrctTag objects.
+        """.select_one that returns a StrctTag object or None if no match is found.
 
-        Perform a CSS selection operation on the current element.
+        Args:
+        ----
+            selector: The string used to select elements from the source.
+            namespaces: The namespaces to use when selecting elements.
+            limit: The maximum number of elements to return. If None, all matching elements are returned.
+            **kwargs: Additional arguments to pass to the parent class's select method.
 
-        This uses the SoupSieve library.
-
-        :param selector: A string containing a CSS selector.
-
-        :param namespaces: A dictionary mapping namespace prefixes
-           used in the CSS selector to namespace URIs. By default,
-           Beautiful Soup will use the prefixes it encountered while
-           parsing the document.
-
-        :param limit: After finding this number of results, stop looking.
-
-        :param kwargs: Keyword arguments to be passed into SoupSieve's
-           soupsieve.select() method.
-
-        :return: A ResultSet of StrctTags.
-        :rtype: bs4.element.ResultSet
+        Returns:
+        -------
+            A ResultSet containing the selected elements, each wrapped in a StrictTag object.
         """
         output = super().select(selector, namespaces, limit, **kwargs)
         return ResultSet[StrictTag](output.source, [StrictTag(item) for item in output])
@@ -56,29 +48,35 @@ class StrictMixin(Tag):
         namespaces: Any | None = None,  # noqa: ANN401 - These types are copied directly from the parent class
         **kwargs: Any,  # noqa: ANN401 - These types are copied directly from the parent class
     ) -> StrictTag | None:
-        """.select_one that returns a StrctTag object or None.
+        """.select_one wrapper that returns a StrctTag object or None.
 
-        :param selector: A CSS selector.
+        Args:
+        ----
+            selector: The string used to select elements from the source.
+            namespaces: The namespaces to use when selecting elements.
+            **kwargs: Additional arguments to pass to the parent class's select method.
 
-        :param namespaces: A dictionary mapping namespace prefixes
-           used in the CSS selector to namespace URIs. By default,
-           Beautiful Soup will use the prefixes it encountered while
-           parsing the document.
-
-        :param kwargs: Keyword arguments to be passed into Soup Sieve's
-           soupsieve.select() method.
-
-        :return: A StrctTag.
-        :rtype: StrctTag
+        Returns:
+        -------
+            A StrictTag object containing the selected element or None if no match is found.
         """
         output = super().select_one(selector, namespaces, **kwargs)
-        if output is None:
-            return output
+        if output is not None:
+            return StrictTag(output)
 
-        return StrictTag(output)
+        return output
 
     def strict_select(self, selector: str) -> ResultSet[StrictTag]:
-        """.select with simplified parameters that will raise an exception if no matches are found."""
+        """.select with simplified parameters that will raise an exception if no matches are found.
+
+        Args:
+        ----
+            selector: The string used to select elements from the source.
+
+        Returns:
+        -------
+            A ResultSet containing the selected elements, each wrapped in a StrictTag object.
+        """
         output = self.select(selector)
         if len(output) == 0:
             msg = f"No matches found for strict_select({selector})"
@@ -87,7 +85,16 @@ class StrictMixin(Tag):
         return output
 
     def strict_select_one(self, selector: str) -> StrictTag:
-        """.select_one with simplified parameters that will raise an exception if no matches are found."""
+        """.select_one with simplified parameters that will raise an exception if no matches are found.
+
+        Args:
+        ----
+            selector: The string used to select elements from the source.
+
+        Returns:
+        -------
+            A StrictTag object containing the selected element.
+        """
         output = self.strict_select(selector)
         if len(output) != 1:
             msg = f"Found {len(output)} matches for strict_select_one({selector})"
@@ -96,20 +103,34 @@ class StrictMixin(Tag):
         return output[0]
 
     def strict_get(self, key: str) -> str:
-        """.get that will raise an exception if no matches are found."""
-        output = self.get(key)
-        if isinstance(output, str):
-            return output
+        """.get that will raise an exception if no matches are found.
 
-        msg = f"No matches found for strict_get({key})"
-        raise StrictSelectError(msg)
+        Args:
+        ----
+            key: The string used to select attributes from the element.
+
+        Returns:
+        -------
+            A string object containing the selected attribute.
+        """
+        output = self.get(key)
+        if not isinstance(output, str):
+            msg = f"No matches found for strict_get({key})"
+            raise StrictSelectError(msg)
+
+        return output
 
 
 class StrictTag(StrictMixin):
     """Tag object that has been extended with extra functions."""
 
     def __init__(self, tag: Tag | None) -> None:
-        """Initialize the StrctTag object."""
+        """Initialize the StrctTag object.
+
+        Args:
+        ----
+            tag: The Tag object to wrap.
+        """
         # Dynamically copy all attributes from the parent class to the child class
         parent_attributes = vars(tag)
         for attr_name, attr_value in parent_attributes.items():
