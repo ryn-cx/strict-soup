aff06cc5816d78125b3ba771d74a1396cf08ab3b
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2023-12-29 00:29:10 -0800

    Added more tests and improved existing ones

--- tests/test_strict_soup.py
+++ tests/test_strict_soup.py
@@ -3,48 +3,67 @@ import pytest
 
 from strict_soup import StrictSelectError, StrictSoup
 
-PARSED_HTML = StrictSoup('<h1 value="123">Test</h1><h2>Test</h2><h2>Test</h2>')
+PARSED_HTML = StrictSoup(
+    '<h1 value="123"><text>H1 Test</text></h1><h2><text>H2 Test 1</text></h2><h2>H2 Text 2</h2>',
+    "html.parser",
+)
 
 
 class TestStrictSelect:
     """Tests for the strict_select function."""
 
-    def test_strict_select(self) -> None:
+    def test_multiple_matches(self) -> None:
         """Test that strict_select returns the correct value."""
-        assert str(PARSED_HTML.strict_select("h2")) == "[<h2>Test</h2>, <h2>Test</h2>]"
+        result = PARSED_HTML.strict_select("h2")
+        assert str(result) == "[<h2><text>H2 Test 1</text></h2>, <h2>H2 Text 2</h2>]"
 
-    def test_strict_select_failure(self) -> None:
+    def test_single_match(self) -> None:
+        """Test that strict_select returns the correct value."""
+        result = PARSED_HTML.strict_select("h1")
+        assert str(result) == '[<h1 value="123"><text>H1 Test</text></h1>]'
+
+    def test_no_matches(self) -> None:
         """Test that strict_select raises an exception when no matches are found."""
         with pytest.raises(StrictSelectError):
             PARSED_HTML.strict_select_one("h3")
+
+    def test_child_matches(self) -> None:
+        """Test that strict_select returns the correct value."""
+        result = PARSED_HTML.strict_select("h2")[0].strict_select("text")
+        assert str(result) == "[<text>H2 Test 1</text>]"
 
 
 class TestStrictSelectOne:
     """Tests for the strict_select_one function."""
 
-    def test_success(self) -> None:
+    def testsingle_match(self) -> None:
         """Test that strict_select_one returns the correct value."""
-        assert str(PARSED_HTML.strict_select_one("h1")) == '<h1 value="123">Test</h1>'
+        assert str(PARSED_HTML.strict_select_one("h1")) == '<h1 value="123"><text>H1 Test</text></h1>'
 
-    def test_failure_no_values(self) -> None:
+    def test_no_matces(self) -> None:
         """Test that strict_select_one raises an exception when no matches are found."""
         with pytest.raises(StrictSelectError):
             PARSED_HTML.strict_select_one("h3")
 
-    def test_failure_too_many_values(self) -> None:
+    def test_multiple_matches(self) -> None:
         """Test that strict_select_one raises an exception when too many matches are found."""
         with pytest.raises(StrictSelectError):
             PARSED_HTML.strict_select_one("h2")
+
+    def test_child_matches(self) -> None:
+        """Test that strict_select returns the correct value."""
+        result = PARSED_HTML.strict_select("h2")[0].strict_select_one("text")
+        assert str(result) == "<text>H2 Test 1</text>"
 
 
 class TestStrictGet:
     """Tests for the strict_get function."""
 
-    def test_success(self) -> None:
+    def test_match(self) -> None:
         """Test that strict_get returns the correct value."""
         assert str(PARSED_HTML.strict_select_one("h1").strict_get("value")) == "123"
 
-    def test_failure(self) -> None:
+    def test_no_match(self) -> None:
         """Test that strict_get raises an exception when no matches are found."""
         tag = PARSED_HTML.strict_select_one("h1")
         with pytest.raises(StrictSelectError):
